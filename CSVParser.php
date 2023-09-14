d882c943f6ef50602022550d99f04992be9b96d8
Nima Aziminejad <nima.a.coder@gmail.com>
2023-09-14 17:48:05 +1000

    feat: Enhance CSVParser with database connection and error handling
    In CSVParser.php, extended the CSVParser class with the following features:
    
    1. Added a private `connectToDatabase` function responsible for creating a MySQL database connection instance.
    
    2. Introduced a private `closeConnection` function to gracefully close the database connection when necessary.
    
    3. Implemented a `showError` function to handle and report errors that may occur during CSV processing. Errors are displayed on STDOUT for user visibility.
    
    4. Enhanced the `run` method to include logic for handling the `create_table` flag. If this flag is provided, the script will create the required database table, improving project flexibility.

--- CSVParser.php
+++ CSVParser.php
@@ -24,10 +24,39 @@ class CSVParser
         if (isset($this->options['help'])) {
             $this->help();
         }
+        $this->connectToDatabase();
+        $this->connection->query("CREATE DATABASE IF NOT EXISTS parserDB");
+        $this->connection->query("USE parserDB");
+        if (isset($this->options['create_table'])) {
+            $this->connection->query("CREATE TABLE IF NOT EXISTS users (
+                                        name VARCHAR(255),
+                                        surname VARCHAR(255),
+                                        email VARCHAR(255) UNIQUE
+                                )");
+            $this->closeConnection();
+            exit(0);
+        }
     }
     private function help()
     {
         echo $this->description;
         exit(0);
+    }
+    private function connectToDatabase()
+    {
+        try {
+            $this->connection = new mysqli($this->host, $this->username, $this->password);
+        } catch (Exception $exception) {
+            $this->showError($exception->getMessage());
+        }
+    }
+    private function showError($msg)
+    {
+        fwrite(STDOUT, "Error: $msg\n");
+        exit(1);
+    }
+    private function closeConnection()
+    {
+        $this->connection->close();
     }
 }
