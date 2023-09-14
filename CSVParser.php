92a3c4be568a213bded10d0c930367cf4593f417
Nima Aziminejad <nima.a.coder@gmail.com>
2023-09-14 17:23:06 +1000

    refactor: Define variables and constructor for CSVParser class
    In CSVParser.php, implemented the following changes in the CSVParser class:
    
    - Defined essential private variables for encapsulation, ensuring data integrity and security.
    
    - Created a constructor method to initialize class instances, accepting necessary parameters for configuration.
    
    These enhancements enhance code organization and maintainability while promoting data encapsulation within the CSVParser class.

--- CSVParser.php
+++ CSVParser.php
@@ -2,5 +2,20 @@
 
 class CSVParser
 {
+    private $options;
+    private $description;
+    private $host;
+    private $username;
+    private $password;
+    private $connection;
+    private $expectedHeaders = ['nima', 'surname', 'email'];
 
+    public function __construct($options, $description)
+    {
+        $this->options = $options;
+        $this->description = $description;
+        $this->host = isset($this->options['h']) ? $this->options['h'] : 'localhost';
+        $this->username = isset($this->options['u']) ? $this->options['u'] : 'root';
+        $this->password = isset($this->options['p']) ? $this->options['p'] : '';
+    }
 }
