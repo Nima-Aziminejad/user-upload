e4c2a7000d3946d00004291b9d5f4e0e58fec9c4
Nima Aziminejad <nima.a.coder@gmail.com>
2023-09-14 17:16:39 +1000

    feat: Define flag options for CommandLineOption
    In CommandLineOption.php, defined custom flag options for the user_upload script, enhancing its functionality:
    
    - $shortOptions: Added short options (-u, -p, -h) for specifying MySQL username, password, and host. Users can now provide parameters for these options.
    
    - $longOptions: Introduced long options (--file, --create_table, --dry_run, --help) for more flexible script usage. These options enable users to:
      - Provide a CSV file using the --file flag.
      - Use the --create_table flag to create the required database table.
      - Run a dry run of the script without data insertion using the --dry_run flag.
      - Retrieve help and descriptions of all available flags with the --help flag.
    
    These enhancements make the user_upload script more versatile and user-friendly.

--- CommandLineOption.php
+++ CommandLineOption.php
@@ -1,1 +1,24 @@
 <?php
+$shortOptions = "u:p:h:";
+$longOptions = ["file:","create_table","dry_run","help"];
+$flagDescription = <<<PHP_EOL
+Script Flags:
+1. --file [csv file name]:
+     Name of the CSV file to be parsed.
+     Example Usage: php script.php --file [csv file name]
+2. --create_table:
+     Build the MySQL users table and exit.
+     Example Usage: php script.php --create_table
+3. --dry_run:
+     Run the script without inserting into the DB.
+     Example Usage: php script.php --file [csv file name] --dry_run
+4. -u [MySQL username]:
+     Specifies the MySQL username to be used for database access.
+5. -p [MySQL password]:
+     Specifies the MySQL password to be used for database access.
+6. -h [MySQL host]:
+     Specifies the MySQL host to be used for database access.
+7. --help:
+     Displays help information.
+     Example Usage: php script.php --help
+PHP_EOL;
